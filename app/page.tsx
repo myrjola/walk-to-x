d60347bd7f5a762789dd9881b3ee0b94361fd849
Martin Yrjölä <martin.yrjola@gmail.com>
2022-11-06 16:09:56 +0200

    Move things around in preparation for auth work

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,11 @@
-import Link from "next/link";
-import prisma from "../lib/prisma";
 import { headers } from "next/headers";
-import CreateTeam from "./CreateTeam";
 
 export default async function Page() {
   headers(); // Make this page dynamic
 
-  const teams = await prisma.team.findMany();
-
   return (
     <main>
       <h1>Walk to X</h1>
-      <CreateTeam />
-      <ul>
-        {teams.map((team) => (
-          <li key={team.id}>
-            <Link href={`/team/${team.id}`}>{team.name}</Link>
-          </li>
-        ))}
-      </ul>
     </main>
   );
 }
---
+++ app/teams/CreateTeam.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import client from "../../utils/trpc";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function CreateTeam() {
+  const router = useRouter();
+  const [name, setName] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  return (
+    <form
+      onSubmit={async (event) => {
+        event.preventDefault();
+        setIsSubmitting(true);
+        const team = await client.addTeam.query({ name });
+        setName("");
+        setIsSubmitting(false);
+        router.push(`/team/${team.id}`);
+      }}
+    >
+      <label>
+        New team
+        <input
+          disabled={isSubmitting}
+          name="teamName"
+          onChange={(event) => setName(event.target.value)}
+        />
+      </label>
+      <button disabled={isSubmitting} type="submit">
+        Create
+      </button>
+    </form>
+  );
+}
