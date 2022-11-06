b9e114048bc724c429ebec593464a4a7730a69b0
Martin Yrjölä <martin.yrjola@gmail.com>
2022-11-06 14:18:42 +0200

    Add head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Walk to X</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Fun fitness goals" />
+      <meta name="robots" content="noindex" />
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,6 @@ interface Props {
 export default function RootLayout({ children }: Props) {
   return (
     <html>
-      <head>
-        <title>Walk to X</title>
-      </head>
       <body>{children}</body>
     </html>
   );
