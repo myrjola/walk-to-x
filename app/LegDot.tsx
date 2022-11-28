f8c614b4b3727f9dd75411b3964b5f128417d011
Martin Yrjölä <martin.yrjola@gmail.com>
2022-11-28 13:20:34 +0200

    Fix LegDot border width

--- app/LegDot.tsx
+++ app/LegDot.tsx
@@ -9,7 +9,7 @@ export default function LegDot({ name, meters }: Props) {
   return (
     <div
       style={{ "--left": metersToPx(meters) } as any}
-      className="absolute -bottom-1 left-[var(--left)] h-10 w-10 -translate-x-1/2 translate-y-1/2 rounded-full border-8 border border-gray-600 bg-gray-50"
+      className="absolute -bottom-1 left-[var(--left)] h-10 w-10 -translate-x-1/2 translate-y-1/2 rounded-full border-8 border-gray-600 bg-gray-50"
     >
       <div className="absolute top-9 left-1/2 -translate-x-1/2 text-center font-medium uppercase text-gray-600">
         {name}
